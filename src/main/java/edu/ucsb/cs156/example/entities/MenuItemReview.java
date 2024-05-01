b4a23c0204ad36d2a9dc6eb90dd7f649d9389123
Tejas Vissapragada <tejas.vissa@gmail.com>
2024-05-01 08:25:05 -0700

    TV - added Entity class for MenuItemReview

---
+++ src/main/java/edu/ucsb/cs156/example/entities/MenuItemReview.java
@@ -0,0 +1,30 @@
+package edu.ucsb.cs156.example.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "menuitemreviwew")
+public class MenuItemReview{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private int itemId;
+    private String reviewerEmail;
+    private String stars;
+    private String comments;
+
+
+
+}
