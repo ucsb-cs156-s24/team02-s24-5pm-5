fb961b727c63d1f769c6876c8455c01b0ccbcc06
Gen Tamada <gtamada@ucsb.edu>
2024-05-01 02:49:13 -0700

    gt - added ucsb org controller, tested on local swagger

--- src/main/java/edu/ucsb/cs156/example/repositories/UCSBOrganizationRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBOrganizationRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface UCSBOrganizationRepository extends CrudRepository<UCSBOrganization, Long> {
+public interface UCSBOrganizationRepository extends CrudRepository<UCSBOrganization, String> {
 
 }
