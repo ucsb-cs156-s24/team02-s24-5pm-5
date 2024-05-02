31374cf0aac6354c5d01ca12bfc56f9821ec6a20
Tejas Vissapragada <tejas.vissa@gmail.com>
2024-05-01 18:03:55 -0700

    TV - undid unnecessary changes

--- src/main/java/edu/ucsb/cs156/example/repositories/ArticlesRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/ArticlesRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ArticlesRepository extends CrudRepository<Articles, Long> {
+public interface ArticlesRepository extends CrudRepository<Articles, String> {
 }
