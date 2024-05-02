0feda143eab690100367b8634ba6650f92a8cd4d
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2024-05-02 00:10:17 -0700

    init

--- src/main/java/edu/ucsb/cs156/example/controllers/MenuItemReviewController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/MenuItemReviewController.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs156.example.controllers;
 
 import edu.ucsb.cs156.example.entities.MenuItemReview;
+import edu.ucsb.cs156.example.entities.UCSBDate;
 import edu.ucsb.cs156.example.entities.UCSBDiningCommonsMenuItems;
 import edu.ucsb.cs156.example.errors.EntityNotFoundException;
 import edu.ucsb.cs156.example.repositories.MenuItemReviewRepository;
@@ -80,5 +81,17 @@ public class MenuItemReviewController extends ApiController{
                 .orElseThrow(() -> new EntityNotFoundException(MenuItemReview.class, id));
 
         return rev;
+    }
+
+    @Operation(summary= "Delete a menu")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("")
+    public Object deleteUCSBDate(
+            @Parameter(name="id") @RequestParam Long id) {
+        MenuItemReview ucsbDate = menuItemReviewRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(UCSBDate.class, id));
+
+        menuItemReviewRepository.delete(ucsbDate);
+        return genericMessage("UCSBDate with id %s deleted".formatted(id));
     }
 }
